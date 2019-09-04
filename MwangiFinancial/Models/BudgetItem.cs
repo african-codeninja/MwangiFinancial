0c9c457c992d14f1fc29dec61b0c4a7324174a90
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-09-04 14:30:03 -0400

    Update BudgetItem.cs
    editing my budget item model

--- MwangiFinancial/Models/BudgetItem.cs
+++ MwangiFinancial/Models/BudgetItem.cs
@@ -7,7 +7,10 @@ namespace MwangiFinancial.Models
 {
     public class BudgetItem
     {
+        //Primary Key
         public int Id { get; set; }
+
+        //Foreign Keys
         public int BudgetId { get; set; }
         public int TransactionId { get; set; }
         public string ItemName { get; set; }
