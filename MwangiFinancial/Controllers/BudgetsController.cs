70070df03211841e01f557f19290ed5a25134b27
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-09-07 04:02:51 -0400

    Create BudgetsController.cs
    creating a controller for my financial portal app

---
+++ MwangiFinancial/Controllers/BudgetsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MwangiFinancial.Models;
+
+namespace MwangiFinancial.Controllers
+{
+    public class BudgetsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Budgets
+        public ActionResult Index()
+        {
+            var myBudget = db.MyBudget.Include(b => b.Household);
+            return View(myBudget.ToList());
+        }
+
+        // GET: Budgets/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Budget budget = db.MyBudget.Find(id);
+            if (budget == null)
+            {
+                return HttpNotFound();
+            }
+            return View(budget);
+        }
+
+        // GET: Budgets/Create
+        public ActionResult Create()
+        {
+            ViewBag.HouseholdId = new SelectList(db.Households, "Id", "Name");
+            return View();
+        }
+
+        // POST: Budgets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,HouseholdId,BudgetItemId,BugetCategoryName,TargetAmount")] Budget budget)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MyBudget.Add(budget);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.HouseholdId = new SelectList(db.Households, "Id", "Name", budget.HouseholdId);
+            return View(budget);
+        }
+
+        // GET: Budgets/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Budget budget = db.MyBudget.Find(id);
+            if (budget == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.HouseholdId = new SelectList(db.Households, "Id", "Name", budget.HouseholdId);
+            return View(budget);
+        }
+
+        // POST: Budgets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,HouseholdId,BudgetItemId,BugetCategoryName,TargetAmount")] Budget budget)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(budget).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.HouseholdId = new SelectList(db.Households, "Id", "Name", budget.HouseholdId);
+            return View(budget);
+        }
+
+        // GET: Budgets/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Budget budget = db.MyBudget.Find(id);
+            if (budget == null)
+            {
+                return HttpNotFound();
+            }
+            return View(budget);
+        }
+
+        // POST: Budgets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Budget budget = db.MyBudget.Find(id);
+            db.MyBudget.Remove(budget);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
