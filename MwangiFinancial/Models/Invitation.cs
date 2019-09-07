31f5eaaad541859a2cacff8aa4befdcc7ba1fe7d
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-09-07 03:57:50 -0400

    Update Invitation.cs
    Carving in my template for the financial app by adding links to folders in the shared layout by dragging link on the page

--- MwangiFinancial/Models/Invitation.cs
+++ MwangiFinancial/Models/Invitation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -16,8 +17,13 @@ namespace MwangiFinancial.Models
         //Structure
         public bool used { get; set; }
         public string Code { get; set; }
+        [Required(ErrorMessage = "Please type in an invitation")]
+        [Display(Name = "Invitation Message")]
+        [StringLength(200, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 10)]
         public string Body { get; set; }
+        [EmailAddress]
         public string ReciepientEmail { get; set; }
+        [EmailAddress]
         public string SenderEmail { get; set; }
         public DateTimeOffset Created { get; set; }
 
