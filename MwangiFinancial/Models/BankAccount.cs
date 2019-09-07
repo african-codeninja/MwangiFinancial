ba162de609ea8cd442514dabe015c0f8173901e0
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-09-07 03:59:05 -0400

    Update BankAccount.cs
    Model creation for the financial app

--- MwangiFinancial/Models/BankAccount.cs
+++ MwangiFinancial/Models/BankAccount.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MwangiFinancial.Enumeration;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,26 +14,34 @@ namespace MwangiFinancial.Models
 
         //Foreign Keys
         public int HouseholdId { get; set; }
-        public int BankAccountTypeId { get; set; }
-        public int TransactionId { get; set; }            
         public string OwnerUserId { get; set; }
 
         //Structure
+        [Required(ErrorMessage ="Please Enter a name for your Account")]
+        [Display(Name = "Account Name")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please choose an account type")]
+        [Display(Name = "Account Type")]
+        public BankAccountType Type { get; set; }
+        [Display(Name = "Starting Balance")]
+        [Range(0.0, 10000000)]
         public decimal StartingBalance { get; set; }
+        [Display(Name = "Low Balance Level")]
+        [Range(0.0, 10000000)]
         public decimal LowBalance { get; set; }
+        [Display(Name = "Current Balance")]
+        [Range(0.0, 10000000)]
         public decimal CurrentBalance { get; set; }
 
-        //Nav
+        //Nav to Parent Household
         public virtual Household Household { get; set; }
-        public virtual BankAccountType BankAccountType { get; set; }
-        public virtual Transaction Transaction { get; set; }
-        public virtual ApplicationUser OwnerUser { get; set; }
 
-
-
+        //Parent of: Transaction and Enum Bank Account Types
         public virtual ICollection<Transaction> Transactions { get; set; }
         public virtual ICollection<BankAccountType> BankAccountTypes { get; set; }
+
+
         public BankAccount()
         {
             Transactions = new HashSet<Transaction>();
